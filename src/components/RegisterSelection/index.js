d133b4964a60b049ec7d0f446c6bf75211e25bb8
Jonathan Nicholas <jojonicho181@gmail.com>
2020-01-08 17:53:22 +0700

    /daftar layout and animations

--- src/components/RegisterSelection/index.js
+++ src/components/RegisterSelection/index.js
@@ -1,22 +1,52 @@
-import React from "react";
+import React, { Fragment } from "react";
 // import PropTypes from 'prop-types';
-
 import { Link } from "react-router-dom";
+import Fade from "react-reveal/Fade";
 import { RegisterSelectionContainer } from "./style";
 import HeaderFooter from "../HeaderFooter";
 
 class RegisterSelection extends React.Component {
+  state = {
+    games: [
+      "futsal",
+      "dota 2",
+      "csgo",
+      "mobile legend",
+      "super smash bros ultimate",
+      "mario kart 8 deluxe",
+      "fifa 20",
+      "catur",
+      "codenames"
+    ],
+    id: [...Array(9).keys()],
+    color: ["purple", "yellow", "orange", "green"],
+    counter: Math.floor(Math.random() * 4)
+  };
+
   render() {
     return (
       <HeaderFooter color="blue">
         <RegisterSelectionContainer>
           <h1 className="title">DAFTARKAN DIRIMU SEKARANG</h1>
-          <h1>PILIH PERMAINANMU</h1>
-          <div className="game-container">
-            <Link to="/daftar/futsal" className="games green">
-              <h1>FUTSAL</h1>
-            </Link>
-          </div>
+          <h1 className="choose">PILIH PERMAINANMU</h1>
+          <Fade top cascade distance="100px" duration={1250}>
+            <div className="game-container">
+              {this.state.id.map(id => {
+                if (this.state.counter < 3) this.state.counter += 1;
+                else this.state.counter = 0;
+                return (
+                  <Link
+                    to={`/daftar/${this.state.games[id]}`}
+                    className={`games ${this.state.color[this.state.counter]} ${
+                      this.state.games[id].split(" ")[0]
+                    }`}
+                  >
+                    <h1>{this.state.games[id].toUpperCase()}</h1>
+                  </Link>
+                );
+              })}
+            </div>
+          </Fade>
         </RegisterSelectionContainer>
       </HeaderFooter>
     );
--- src/containers/App/routes.js
+++ src/containers/App/routes.js
@@ -1,18 +1,19 @@
 // import Registration from "../Registration";
 // import RegisterSelection from "../../components/RegisterSelection";
 import LandingPage from "../LandingPage";
+import RegisterSelection from "../../components/RegisterSelection";
 
 export const routes = [
   {
     component: LandingPage,
     exact: true,
     path: "/"
+  },
+  {
+    component: RegisterSelection,
+    exact: true,
+    path: "/daftar"
   }
-  // {
-  //   component: RegisterSelection,
-  //   exact: true,
-  //   path: "/daftar"
-  // },
   // {
   //   component: Registration,
   //   exact: true,
