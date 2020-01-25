f0e55550ec80741734b70e87311c8e59e6921c34
Kefas Satrio Bangkit Solideantyo <kefassatrio@gmail.com>
2020-01-25 21:04:43 +0700

    at this point gw gatau parallax nya bener apa ga

--- src/containers/LandingPage2/index.js
+++ src/containers/LandingPage2/index.js
@@ -9,10 +9,60 @@ import daun2 from "../../asset/daun2.svg";
 import { LandingPage2Container, LandingSection } from "./style";
 
 class LandingPage2 extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      thePosition: 0
+    };
+
+    this.noParallax = () => {
+      // $("#nonparallax").css('display','block');
+      // $("#parallax").css('display','none');
+      const parallaxes = document.getElementsByClassName("parallax");
+      for (let i = 0; i < parallaxes.length; i += 1) {
+        document.getElementById(`layer${i + 1}`).style.display = "none";
+      }
+    };
+
+    this.parallax = () => {
+      this.setState({
+        thePosition: window.pageYOffset
+      });
+      const { thePosition } = this.state;
+      if (thePosition < 600) {
+        const layers = document.getElementsByClassName("parallax");
+        // console.log(layers[3].getAttribute('data-speed'));
+        let { layer, speed, yPos } = 0;
+        for (let i = 0; i < layers.length; i += 1) {
+          layer = layers[i];
+          speed = layer.getAttribute("data-speed");
+          yPos = -((thePosition * speed) / 100);
+          layer.setAttribute(
+            "style",
+            `transform: translate3d(0px, ${yPos}px, 0px)`
+          );
+        }
+      }
+    };
+  }
+
+  componentDidMount() {
+    const platform = navigator.platform.toLowerCase();
+    if (platform.indexOf("ipad") !== -1 || platform.indexOf("iphone") !== -1) {
+      this.noParallax();
+    } else {
+      window.addEventListener("scroll", this.parallax);
+    }
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.parallax);
+  }
+
   render() {
     return (
       <LandingPage2Container>
-        <HeaderFooter color="dark">
+        <HeaderFooter color="notShown">
           <div className="leaf">
             <img src={daun2} alt="daun2" />
           </div>
@@ -42,18 +92,31 @@ class LandingPage2 extends React.Component {
           </div>
 
           <LandingSection className="landing">
-            <div className="title">
-              <h1>
-                PESTA <span>RAKYAT</span>
-              </h1>
-              <h1>KOMPUTER</h1>
+            <div className="layer parallax" data-speed={60} id="layer1"></div>
+            <div className="layer parallax" data-speed={-20} id="layer2">
+              <div className="title">
+                <h1>
+                  PESTA <span>RAKYAT</span>
+                </h1>
+                <h1>KOMPUTER</h1>
+              </div>
             </div>
-            <div className="layer" id="layer1"></div>
-            <div className="layer parallax" id="layer2"></div>
-            <div className="layer parallax" id="layer3"></div>
-            <div className="layer parallax" id="layer4"></div>
-            <div className="layer parallax" id="layer5"></div>
+            <div className="layer parallax" data-speed={-40} id="layer3"></div>
+            <div className="layer parallax" data-speed={-80} id="layer4"></div>
+            <div className="layer parallax" data-speed={-120} id="layer5"></div>
+            <div className="layer parallax" data-speed={-80} id="layer6"></div>
           </LandingSection>
+          <section
+            style={{
+              height: "100vh",
+              backgroundColor: "white",
+              marginTop: "-1px",
+              position: "relative",
+              zIndex: 7
+            }}
+          >
+            <h1>TEST</h1>
+          </section>
         </HeaderFooter>
       </LandingPage2Container>
     );
--- src/containers/LandingPage2/style.js
+++ src/containers/LandingPage2/style.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import landingSectionBG from "../../asset/landing-background.svg";
-import landingLayer1 from "../../asset/landing-layer1.svg";
+import landingLayer1 from "../../asset/landing-layer1-v2.svg";
 import landingLayer2 from "../../asset/landing-layer2.svg";
 import landingLayer3 from "../../asset/landing-layer3.svg";
 import landingLayer3Right from "../../asset/landing-layer3-right.svg";
@@ -51,7 +51,7 @@ export const LandingPage2Container = styled.div`
   .leaf:nth-child(5) {
     left: 70%;
     top: 150px;
-    filter: blur(1px);
+    /* filter: blur(1px); */
     animation: animate-leaves 25s linear infinite;
   }
   .leaf:nth-child(6) {
@@ -112,11 +112,10 @@ export const LandingPage2Container = styled.div`
 export const LandingSection = styled.div`
   width: 100%;
   height: 100vh;
-  background-image: url(${landingSectionBG});
+  overflow-y: hidden;
   background-color: #e5e6de;
-  background-position-y: -30px;
-  background-repeat: no-repeat;
-  background-size: contain;
+  position: relative;
+
   .title {
     position: absolute;
   }
@@ -125,7 +124,6 @@ export const LandingSection = styled.div`
     height: 100vh;
     background-repeat: no-repeat;
     background-position-y: bottom;
-    background-size: contain;
     background-color: transparent;
     position: absolute;
   }
@@ -133,7 +131,10 @@ export const LandingSection = styled.div`
   }
   #layer1 {
     background-image: url(${landingLayer1});
-
+    background-size: cover;
+    height: 200vh;
+    background-position-y: 250px;
+    top: 250px;
     z-index: 5;
   }
   #layer2 {
@@ -141,27 +142,50 @@ export const LandingSection = styled.div`
     background-position-x: right;
     background-position-y: 250px;
     z-index: 4;
-    background-size: 80%;
+    background-size: 1200px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .title {
+      margin-left: 100px;
+      margin-top: 100px;
+      font-family: BigJohnPRO;
+      font-size: 2em;
+      color: white;
+    }
   }
   #layer3 {
     background-image: url(${landingLayer3}), url(${landingLayer3Right});
     z-index: 3;
     background-position-y: 300px, 200px;
     background-position-x: left, right;
-    background-size: 80%, 50%;
+    background-size: 1200px, 750px;
   }
   #layer4 {
     background-image: url(${landingLayer4});
     background-position-y: 270px;
-    background-size: 80%;
+    background-size: 1200px;
     background-position-x: left;
     z-index: 2;
   }
   #layer5 {
     background-image: url(${landingLayer5});
-    background-size: 50%;
+    background-size: 750px;
     background-position-x: 250px;
     background-position-y: 150px;
     z-index: 1;
+  }
+  #layer6 {
+    background-image: url(${landingSectionBG});
+    background-position-y: -30px;
+    background-repeat: no-repeat;
+    background-size: contain;
+  }
+  @media only screen and (max-width: 800px) {
+    .layer,
+    #layer2 {
+      display: none;
+    }
   }
 `;
