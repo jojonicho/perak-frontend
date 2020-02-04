c5a660dfe4b11a56d7b94232ef7d9ad1959f1dc6
Leonardo81 <leonardoeinstein2000@gmail.com>
2020-02-05 00:06:58 +0700

    remove daftar routing

--- src/containers/App/routes.js
+++ src/containers/App/routes.js
@@ -1,9 +1,9 @@
-import React from "react";
-import Registration from "../Registration";
-import RegisterSelection from "../../components/RegisterSelection";
+// import React from "react";
+// import Registration from "../Registration";
+// import RegisterSelection from "../../components/RegisterSelection";
 // import LandingPage from "../LandingPage";
+// import GameRegistration from "../GameRegistration";
 import LandingPage2 from "../LandingPage2";
-import GameRegistration from "../GameRegistration";
 import FutsalScorePage from "../FutsalScorePage";
 import KlasemenPage from "../KlasemenPage";
 
@@ -19,58 +19,58 @@ export const routes = [
     path: "/result/futsal"
   },
   {
-    component: RegisterSelection,
-    exact: true,
-    path: "/daftar"
-  },
-  {
-    component: Registration,
-    exact: true,
-    path: "/daftar/futsal"
-  },
-  {
-    component: () => <GameRegistration gameId="dota" />,
-    exact: true,
-    path: "/daftar/dota"
-  },
-  {
-    component: () => <GameRegistration gameId="csgo" />,
-    exact: true,
-    path: "/daftar/csgo"
-  },
-  {
-    component: () => <GameRegistration gameId="mlbb" />,
-    exact: true,
-    path: "/daftar/mlbb"
-  },
-  {
-    component: () => <GameRegistration gameId="ssbu" />,
-    exact: true,
-    path: "/daftar/ssbu"
-  },
-  {
-    component: () => <GameRegistration gameId="mariokart" />,
-    exact: true,
-    path: "/daftar/mariokart"
-  },
-  {
-    component: () => <GameRegistration gameId="fifa" />,
-    exact: true,
-    path: "/daftar/fifa"
-  },
-  {
-    component: () => <GameRegistration gameId="codenames" />,
-    exact: true,
-    path: "/daftar/codenames"
-  },
-  {
-    component: () => <GameRegistration gameId="catur" />,
-    exact: true,
-    path: "/daftar/catur"
-  },
-  {
     component: KlasemenPage,
     exact: true,
     path: "/klasemen"
   }
+  // {
+  //   component: RegisterSelection,
+  //   exact: true,
+  //   path: "/daftar"
+  // },
+  // {
+  //   component: Registration,
+  //   exact: true,
+  //   path: "/daftar/futsal"
+  // },
+  // {
+  //   component: () => <GameRegistration gameId="dota" />,
+  //   exact: true,
+  //   path: "/daftar/dota"
+  // },
+  // {
+  //   component: () => <GameRegistration gameId="csgo" />,
+  //   exact: true,
+  //   path: "/daftar/csgo"
+  // },
+  // {
+  //   component: () => <GameRegistration gameId="mlbb" />,
+  //   exact: true,
+  //   path: "/daftar/mlbb"
+  // },
+  // {
+  //   component: () => <GameRegistration gameId="ssbu" />,
+  //   exact: true,
+  //   path: "/daftar/ssbu"
+  // },
+  // {
+  //   component: () => <GameRegistration gameId="mariokart" />,
+  //   exact: true,
+  //   path: "/daftar/mariokart"
+  // },
+  // {
+  //   component: () => <GameRegistration gameId="fifa" />,
+  //   exact: true,
+  //   path: "/daftar/fifa"
+  // },
+  // {
+  //   component: () => <GameRegistration gameId="codenames" />,
+  //   exact: true,
+  //   path: "/daftar/codenames"
+  // },
+  // {
+  //   component: () => <GameRegistration gameId="catur" />,
+  //   exact: true,
+  //   path: "/daftar/catur"
+  // },
 ];
