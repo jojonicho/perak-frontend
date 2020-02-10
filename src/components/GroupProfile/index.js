18cad3888842d622b6cefc976049cf34e66bc32c
ganiirsyadi <ganiilhamirsyadi@gmail.com>
2020-02-10 12:17:30 +0700

    Coba fix yang foto pemain biar ga gepeng

--- src/components/GroupProfile/index.js
+++ src/components/GroupProfile/index.js
@@ -2,109 +2,119 @@ import React from "react";
 // import PropTypes from 'prop-types';
 // import Carousel from "react-multi-carousel";
 // import "react-multi-carousel/lib/styles.css";
+// import Carousel from "@brainhubeu/react-carousel";
 import { GroupProfileContainer } from "./style";
 
-import logo from "../../asset/GroupProfile/Logo.svg";
-import person from "../../asset/GroupProfile/Person.jpg";
+import logo from "../../asset/GroupProfile/logo.jpg";
+import person from "../../asset/GroupProfile/Person3.jpg";
+import HeaderFooter from "../HeaderFooter";
+// import "@brainhubeu/react-carousel/lib/style.css";
+// import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 // import HeaderFooter from "../../components/HeaderFooter";
 
 class GroupProfile extends React.Component {
   render() {
     return (
-      <GroupProfileContainer>
-        <div className="profile-group">
-          <div className="container">
-            <img className="logo" src={logo} alt="Logo Tim" />
-            <h1>TUTUY FC</h1>
-            <div className="line"></div>
-            <h3>MANAGER: Alvia Dibby</h3>
-            <div className="card-container">
-              <div className="card">
-                <div className="card-img">
-                  <img src={person} alt="Avatar" />
-                </div>
-                <div className="desc">
-                  <h4>GANI</h4>
-                  <p>MUHAMMAD GANI ILHAM IRSYADI</p>
-                </div>
-              </div>
-              <div className="card">
-                <div className="card-img">
-                  <img src={person} alt="Avatar" />
-                </div>
-                <div className="desc">
-                  <h4>GANI</h4>
-                  <p>MUHAMMAD GANI ILHAM IRSYADI</p>
-                </div>
-              </div>
-              <div className="card">
-                <div className="card-img">
-                  <img src={person} alt="Avatar" />
-                </div>
-                <div className="desc">
-                  <h4>GANI</h4>
-                  <p>MUHAMMAD GANI ILHAM IRSYADI</p>
-                </div>
-              </div>
-              <div className="card">
-                <div className="card-img">
-                  <img src={person} alt="Avatar" />
-                </div>
-                <div className="desc">
-                  <h4>GANI</h4>
-                  <p>MUHAMMAD GANI ILHAM IRSYADI</p>
-                </div>
-              </div>
-              <div className="card">
-                <div className="card-img">
-                  <img src={person} alt="Avatar" />
-                </div>
-                <div className="desc">
-                  <h4>GANI</h4>
-                  <p>MUHAMMAD GANI ILHAM IRSYADI</p>
-                </div>
-              </div>
-              <div className="card">
-                <div className="card-img">
-                  <img src={person} alt="Avatar" />
-                </div>
-                <div className="desc">
-                  <h4>GANI</h4>
-                  <p>MUHAMMAD GANI ILHAM IRSYADI</p>
-                </div>
-              </div>
-              <div className="card">
-                <div className="card-img">
-                  <img src={person} alt="Avatar" />
-                </div>
-                <div className="desc">
-                  <h4>GANI</h4>
-                  <p>MUHAMMAD GANI ILHAM IRSYADI</p>
-                </div>
-              </div>
-              <div className="card">
-                <div className="card-img">
-                  <img src={person} alt="Avatar" />
-                </div>
-                <div className="desc">
-                  <h4>GANI</h4>
-                  <p>MUHAMMAD GANI ILHAM IRSYADI</p>
-                </div>
-              </div>
-              <div className="card">
-                <div className="card-img">
-                  <img src={person} alt="Avatar" />
-                </div>
-                <div className="desc">
-                  <h4>GANI</h4>
-                  <p>MUHAMMAD GANI ILHAM IRSYADI</p>
+      <HeaderFooter color="dark">
+        <GroupProfileContainer>
+          <div className="profile-group">
+            <div className="container">
+              <div className="container-inside">
+                <img className="logo" src={logo} alt="Logo Tim" />
+                <h1>TUTUY FC</h1>
+                <div className="line"></div>
+                <h3>
+                  MANAGER: <span>Alvia Dibby</span>
+                </h3>
+                <div className="card-container">
+                  <div className="card">
+                    <div className="card-img">
+                      <img src={person} alt="Avatar" />
+                    </div>
+                    <div className="desc">
+                      <h4>GANI</h4>
+                      <p>MUHAMMAD GANI ILHAM IRSYADI</p>
+                    </div>
+                  </div>
+                  <div className="card">
+                    <div className="card-img">
+                      <img src={person} alt="Avatar" />
+                    </div>
+                    <div className="desc">
+                      <h4>GANI</h4>
+                      <p>MUHAMMAD GANI ILHAM IRSYADI</p>
+                    </div>
+                  </div>
+                  <div className="card">
+                    <div className="card-img">
+                      <img src={person} alt="Avatar" />
+                    </div>
+                    <div className="desc">
+                      <h4>GANI</h4>
+                      <p>MUHAMMAD GANI ILHAM IRSYADI</p>
+                    </div>
+                  </div>
+                  <div className="card">
+                    <div className="card-img">
+                      <img src={person} alt="Avatar" />
+                    </div>
+                    <div className="desc">
+                      <h4>GANI</h4>
+                      <p>MUHAMMAD GANI ILHAM IRSYADI</p>
+                    </div>
+                  </div>
+                  <div className="card">
+                    <div className="card-img">
+                      <img src={person} alt="Avatar" />
+                    </div>
+                    <div className="desc">
+                      <h4>GANI</h4>
+                      <p>MUHAMMAD GANI ILHAM IRSYADI</p>
+                    </div>
+                  </div>
+                  <div className="card">
+                    <div className="card-img">
+                      <img src={person} alt="Avatar" />
+                    </div>
+                    <div className="desc">
+                      <h4>GANI</h4>
+                      <p>MUHAMMAD GANI ILHAM IRSYADI</p>
+                    </div>
+                  </div>
+                  <div className="card">
+                    <div className="card-img">
+                      <img src={person} alt="Avatar" />
+                    </div>
+                    <div className="desc">
+                      <h4>GANI</h4>
+                      <p>MUHAMMAD GANI ILHAM IRSYADI</p>
+                    </div>
+                  </div>
+                  <div className="card">
+                    <div className="card-img">
+                      <img src={person} alt="Avatar" />
+                    </div>
+                    <div className="desc">
+                      <h4>GANI</h4>
+                      <p>MUHAMMAD GANI ILHAM IRSYADI</p>
+                    </div>
+                  </div>
+                  <div className="card">
+                    <div className="card-img">
+                      <img src={person} alt="Avatar" />
+                    </div>
+                    <div className="desc">
+                      <h4>GANI</h4>
+                      <p>MUHAMMAD GANI ILHAM IRSYADI</p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </GroupProfileContainer>
+        </GroupProfileContainer>
+      </HeaderFooter>
     );
   }
 }
--- src/components/GroupProfile/style.js
+++ src/components/GroupProfile/style.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import background from "../../asset/GroupProfile/background.svg";
+import backgroundPhone from "../../asset/GroupProfile/backgroundPhone.svg";
 
 export const GroupProfileContainer = styled.div`
   * {
@@ -13,78 +14,85 @@ export const GroupProfileContainer = styled.div`
     align-items: center;
   }
   .container {
-    margin: 0 auto;
-    min-height: 80vw;
-    width: 80vw;
+    min-height: 92vh;
+    font-size: 0.8rem;
+  }
+  .container-inside {
+    margin: 4em auto 0 auto;
+    min-height: 75em;
+    width: 75em;
     background-image: url(${background});
     background-size: contain;
     background-repeat: no-repeat;
     display: flex;
     align-items: center;
     flex-direction: column;
-    height: 700px;
     text-align: center;
   }
 
   .logo {
-    margin: 10vw auto 0 auto;
-    width: 17vw;
+    margin: 7em auto 0 auto;
+    width: 17em;
+    height: 17em;
+    border-radius: 50%;
   }
   h1 {
-    font-size: 4vw;
-    margin-top: 3vw;
+    font-size: 4em;
+    margin-top: 0.5em;
     font-family: BigJohnPro;
     color: #444fcb;
   }
   .line {
     height: 0;
     width: 70%;
-    margin: 1.5vw auto;
-    border-bottom: 2px solid #f2cf35;
+    margin: 1em auto;
+    border-bottom: 0.2em solid #f2cf35;
   }
   h3 {
     font-family: Montserrat;
     color: #0d2040;
-    font-size: 2vw;
+    font-size: 2em;
   }
   .card-container {
-    margin: 4vw auto;
-    width: 55vw;
-    height: 25vw;
+    margin: 3em auto;
+    width: 47em;
+    height: 23em;
     white-space: nowrap;
     overflow-x: scroll;
     overflow-y: hidden;
   }
   .card {
     display: inline-block;
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-    width: 18vw;
-    margin: 0 1vw;
+    box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.2);
+    width: 15em;
+    margin: 0 1.6em 0 0;
     background-color: #ffffff;
   }
 
   .card-img {
-    height: 16vw;
+    height: 14em;
     width: 100%;
   }
   .card-img img {
-    width: 100%;
     height: 100%;
+    width: 100%;
+    object-fit: cover;
   }
   .desc {
     display: flex;
     flex-direction: column;
     justify-content: center;
-    height: 7vw;
+    height: 6em;
     width: 100%;
-    padding: 0 2vw;
+    padding: 0 1em;
     background-color: #f2cf35;
-    font-size: 1.1vw;
+    font-size: 0.9em;
     white-space: normal;
+    line-height: 1.3;
   }
   .desc h4 {
-    font-size: 1.5vw;
-    margin-bottom: 0.5vw;
+    font-size: 1.2em;
+    margin-bottom: 0.2em;
   }
 
   .card-container::-webkit-scrollbar-track {
@@ -93,12 +101,78 @@ export const GroupProfileContainer = styled.div`
   }
 
   .card-container::-webkit-scrollbar {
-    width: 10px;
+    width: 2px;
+    height: 5px;
     background-color: #f5f5f5;
   }
 
   .card-container::-webkit-scrollbar-thumb {
     background-color: #f2cf35;
-    border: 2px solid #f2cf35;
+    border: 0.5px solid #f2cf35;
+  }
+
+  @media only screen and (max-width: 800px) {
+    .container {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+    }
+    .container-inside {
+      font-size: 1.2vw;
+      margin-top: 0;
+    }
+  }
+
+  @media only screen and (max-width: 430px) {
+    .container-inside {
+      background: url(${backgroundPhone});
+      background-repeat: no-repeat;
+      font-size: 1.3vw;
+      margin: 2em 0;
+      width: 100vw;
+      height: 190vw;
+      background-size: contain;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+    }
+    .logo {
+      margin-top: -2em;
+      width: 38em;
+      height: 38em;
+      padding: 1.5em;
+    }
+    h1 {
+      font-size: 7em;
+      margin-top: 1em;
+    }
+    .line {
+      margin-top: 1em;
+      width: 45em;
+      border-bottom: 0.7em solid #f2cf35;
+    }
+    h3 {
+      margin-top: 0.6em;
+      font-size: 3.4em;
+      line-height: 1.4;
+    }
+    h3 span {
+      font-size: 1.5em;
+      display: block;
+    }
+    .card-container {
+      margin-top: 4em;
+      height: 35em;
+      width: 50em;
+    }
+    .card {
+      width: 20em;
+    }
+    .card-img {
+      height: 20em;
+    }
+    .desc {
+      font-size: 1.7em;
+    }
   }
 `;
