a824b878f95756282489cadb133bd80352bc36c6
Andrew <susanto.andrew@ymail.com>
2020-02-13 10:03:01 +0700

    add transition

--- src/components/HeaderFooter/style.js
+++ src/components/HeaderFooter/style.js
@@ -53,64 +53,71 @@ export const HeaderFooterContainer = styled.div`
     color: #e5e6de;
     transform: translateY(+100%);
   }
-.menu-bar{
-  display:flex;
-  flex-direction:row;
-}
-.dropdown{
-  padding-left:25px;
-  padding-right:25px;
-  position:relative;
-  display:inline-block;
-  height:60px
-}
-.dropdown-menu{
-  display:none;
-  flex-direction:row;
-  width:max-content;
-  background-color:rgba(7,21,46,0.94);
-  margin:-right:50px;
-  margin-left:50px;
-  padding:20px;
-}
-.dropdown-menu-klasemen{
-  margin-left:0px
-}
-.dropdown:hover .dropdown-menu-klasemen{
-  right:0%!important;
-}
-.dropdown-col-klasemen{
-  width:130px!important;
-}
-.dropdown:hover .dropdown-menu{
-  display:flex;
-  position:absolute;
-  right:-50%;
-  top:100%;
-}
-.dropdown:hover{
-  background-color:rgba(7,21,46,1);
-}
-.dropdown-col{
-  display:flex;
-  flex-direction:column;
-  width:45%;
-}
-.dropdown-col-left{
-  border-right-width:2px;
-  border-right-style:solid;
-  border-right-color:gray;
-}
+  .menu-bar {
+    display: flex;
+    flex-direction: row;
+  }
+  .dropdown {
+    padding-left: 25px;
+    padding-right: 25px;
+    position: relative;
+    display: inline-block;
+    height: 60px;
+  }
+  .dropdown-menu {
+    position: absolute;
+    display: flex;
+    flex-direction: row;
+    right: -20%;
+    top: 100%;
+    width: max-content;
+    background-color: rgba(7, 21, 46, 0.94);
+    height: 0px;
+    overflow: hidden;
+    padding-top: 0px;
+    padding-bottom: 0px;
+    transition: height 1s, padding-top 1s, padding-bottom 1s;
+  }
+  .dropdown:hover .dropdown-menu {
+    height: 270px !important;
+    padding-top: 20px;
+    padding-bottom: 20px;
+  }
+  .dropdown:hover {
+    background-color: rgba(7, 21, 46, 1);
+  }
+  .dropdown-menu-klasemen {
+    right: 0% !important;
+  }
+  .dropdown:hover .dropdown-menu-klasemen {
+    height: 200px !important;
+  }
+  .dropdown-col-klasemen {
+    width: 150px !important;
+  }
+  .dropdown-col {
+    display: flex;
+    flex-direction: column;
+    width: 200px;
+  }
+  .dropdown-col-left {
+    border-right-width: 2px;
+    border-right-style: solid;
+    border-right-color: gray;
+  }
 
-.button-menu{
-  margin:15px;
-  color:#E5E6DE;
-  text-align:center;
-}
-.dropdown-item{
-  margin-left:auto;
-  margin-right:auto;
-}
+  .button-menu {
+    padding: 15px;
+    color: #e5e6de;
+    text-align: center;
+  }
+  .button-menu:hover {
+    background-color: #07152e;
+  }
+  .dropdown-item {
+    margin-left: auto;
+    margin-right: auto;
+  }
   @media only screen and (max-width: 350px) {
     .logo {
       height: 35px;
