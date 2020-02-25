01761a2f9c8c85ecbe926437aa57e73194975e76
Leonardo81 <leonardoeinstein2000@gmail.com>
2020-02-25 20:49:26 +0700

    cleanup bazar

--- src/components/OpenTenantForm/index.js
+++ src/components/OpenTenantForm/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 // import PropTypes from "prop-types";
+import Fade from "react-reveal/Fade";
+
 import {
   PersonFormRegistrationContainer,
   Section
@@ -20,97 +22,107 @@ class OpenTenantForm extends React.Component {
     } = this.props;
     return (
       <PersonFormRegistrationContainer>
-        <OpenTenantFormContainer>
-          <Section>
-            Nama
-            <input
-              className="input-long"
-              id="nama-pemilik"
-              value={namaPemilik}
-              onChange={setData}
-              autoComplete="off"
-            />
-          </Section>
-          <Section>
-            Kontak (WhatsApp)
-            <input
-              className="input-long"
-              id="kontak-pemilik"
-              value={kontak}
-              onChange={setData}
-              autoComplete="off"
-            />
-          </Section>
-          <Section>
-            Jenis yang akan dijual
-            <div className="check-box-container">
-              <div className="check-box">
-                <input
-                  type="radio"
-                  id="makanan"
-                  name="jenis"
-                  value="Makanan"
-                  onChange={setData}
-                />
-                <label htmlFor="makanan">Makanan</label>
-                <br />
-                <input
-                  type="radio"
-                  id="minuman"
-                  name="jenis"
-                  value="Minuman"
-                  onChange={setData}
-                />
-                <label htmlFor="minuman">Minuman</label>
-                <br />
-                <input
-                  type="radio"
-                  id="Other"
-                  name="jenis"
-                  value="other"
-                  onChange={setData}
-                />
-                <label htmlFor="Other">Other</label>
-              </div>
-            </div>
-            <input
-              className={`input-long ${
-                jenisJualan === "other" ? null : "hide"
-              }`}
-              id="input-other"
-              value={other}
-              onChange={setData}
-              autoComplete="off"
-            />
-          </Section>
-          <Section>
-            Deskripsi singkat mengenai apa yang ingin dijual
-            <input
-              className="input-long"
-              id="deskripsi-jualan"
-              value={deskripsi}
-              onChange={setData}
-              autoComplete="off"
-            />
-          </Section>
-          <Section>
-            <p>
-              Saya bersedia untuk membayar commitment fee Rp100.000,- (Sudah
-              termasuk DP)
-            </p>
-            <div className="check-box-container">
+        <Fade>
+          <OpenTenantFormContainer>
+            <Section>
+              Nama
               <input
-                type="checkbox"
-                id="setuju"
-                checked={setuju}
+                className="input-long"
+                id="nama-pemilik"
+                value={namaPemilik}
                 onChange={setData}
                 autoComplete="off"
-                className="bazar-agree"
               />
-              <label htmlFor="setuju">Setuju</label>
-            </div>
-          </Section>
-        </OpenTenantFormContainer>
+            </Section>
+            <Section>
+              Kontak (WhatsApp)
+              <input
+                className="input-long"
+                id="kontak-pemilik"
+                value={kontak}
+                onChange={setData}
+                autoComplete="off"
+              />
+            </Section>
+            <Section>
+              Jenis yang akan dijual
+              <div className="check-box-container">
+                <div className="check-box">
+                  <label htmlFor="makanan">
+                    <input
+                      type="radio"
+                      id="makanan"
+                      name="jenis"
+                      value="Makanan"
+                      onChange={setData}
+                    />
+                    Makanan
+                  </label>
+                  <br />
+                  <label htmlFor="minuman">
+                    <input
+                      type="radio"
+                      id="minuman"
+                      name="jenis"
+                      value="Minuman"
+                      onChange={setData}
+                    />
+                    Minuman
+                  </label>
+                  <br />
+                  <label htmlFor="Other" className="flex-label">
+                    <input
+                      type="radio"
+                      id="Other"
+                      name="jenis"
+                      value="other"
+                      onChange={setData}
+                    />
+                    <p className="p-marg">Other</p>
+                    <input
+                      className={`input-long ${
+                        jenisJualan === "other" ? null : "hide"
+                      }`}
+                      id="input-other"
+                      value={other}
+                      onChange={setData}
+                      autoComplete="off"
+                    />
+                  </label>
+                </div>
+              </div>
+            </Section>
+            <Section>
+              Deskripsi singkat mengenai apa yang ingin dijual
+              <input
+                className="input-long"
+                id="deskripsi-jualan"
+                value={deskripsi}
+                onChange={setData}
+                autoComplete="off"
+              />
+            </Section>
+            <Section>
+              <p>
+                Saya bersedia untuk membayar commitment fee Rp100.000,- (Sudah
+                termasuk DP)
+              </p>
+              <div className="check-box-container">
+                <label htmlFor="setuju">
+                  <input
+                    type="checkbox"
+                    id="setuju"
+                    checked={setuju}
+                    onChange={setData}
+                    autoComplete="off"
+                    className="bazar-agree"
+                  />
+                  Setuju
+                </label>
+              </div>
+            </Section>
+          </OpenTenantFormContainer>
+        </Fade>
       </PersonFormRegistrationContainer>
     );
   }
--- src/components/OpenTenantForm/style.js
+++ src/components/OpenTenantForm/style.js
@@ -5,8 +5,9 @@ export const OpenTenantFormContainer = styled.div`
     display: flex;
   }
 
-  .check-box input {
+  .check-box input[type="radio"] {
     width: 20px;
+    height: 15px;
     min-width: 10px;
     box-shadow: none;
     margin: 0;
@@ -14,10 +15,12 @@ export const OpenTenantFormContainer = styled.div`
 
   .check-box-container {
     margin-top: 10px;
+    padding-left: 10px;
   }
 
   .bazar-agree {
     width: 30px;
+    height: 18px;
     min-width: 10px;
     box-shadow: none;
     margin: 0 !important;
@@ -25,5 +28,22 @@ export const OpenTenantFormContainer = styled.div`
 
   .hide {
     display: none;
+
+    .check-box label {
+      cursor: pointer !important;
+    }
+  }
+  .flex-label {
+    display: flex;
+  }
+
+  .p-marg {
+    margin-top: 8px;
+    margin-right: 10px;
+  }
+
+  #input-other {
+    margin-top: 2px !important;
+    width: 70%;
   }
 `;
